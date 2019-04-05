08ad64363a698272880495f4d3b89054ae13f021
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2019-04-05 20:40:08 +0200

    carousel component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Layout } from "./components";
+import ControlledCarousel from "./components/ControlledCarousel";
+import TryForFreeBtn from "./components/TryForFreeBtn";
 
 // classed based component, extends Component class
 class App extends Component {
   // ono sta se vidi u browseru
   render() {
-    return <Layout>main</Layout>;
+    return (
+      <Layout>
+        <ControlledCarousel />
+        <br />
+        <h2 align="center">
+          <font color="white">The best series online</font>
+        </h2>
+        <TryForFreeBtn />
+      </Layout>
+    );
   }
 }
 
---
+++ src/components/TryForFreeBtn/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export default () => (
+  <div className={styles.TryForFree}>
+    <button type="button" className="btn btn-warning" align="center">
+      <strong>TRY FOR FREE</strong>
+    </button>
+  </div>
+);
